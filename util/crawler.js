6fee8e2bcec24468b778561147893e217a937fa2
XxshiftxX <qjfrntop12@naver.com>
2019-04-01 14:06:22 +0900

    Add direct hosting crawler

--- util/crawler.js
+++ util/crawler.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
+const cheerio = require('cheerio');
 
-const domain = 'bvr678ijbvftyujnbvtyujn.com';
+const domain = 'bvr678ijbvftyujnbvtyujn';
 
 const godaddy = async () => {
   const { data: { Products } } = await axios({
@@ -16,5 +17,26 @@ const godaddy = async () => {
     },
   });
   return Products
-    .map(({ Tld, PriceInfo: { CurrentPrice } }) => ({ tld: Tld, price: CurrentPrice }));
+    .map(({ Tld: tld, PriceInfo: { CurrentPrice: price } }) => ({ tld, price }));
+};
+
+const directHosting = async () => {
+  const { data: lawHTML } = await axios.get('https://direct.co.kr/domain/dm_pay.html');
+  const $ = cheerio.load(lawHTML);
+
+  const res = $('div[class=con] tbody')
+    .first()
+    .find('tr').map((i, e) => ({
+      tld: $(e).find('th').text(),
+      price: $(e).find('td').text().slice(0, -3),
+    }))
+    .get();
+
+  return res;
+};
+
+
+module.exports = {
+  godaddy,
+  directHosting,
 };
