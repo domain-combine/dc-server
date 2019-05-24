7f3183cb0b1ee15a0070684bbd08f644ae9a7b0e
XxshiftxX <qjfrntop12@naver.com>
2019-05-24 10:37:03 +0900

    Resolve review

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const ctrl = require('./index.ctrl');
 
 const router = express.Router();
 
-router.get('/list/:domain', ctrl.getList);
-router.get('/detail/:tld', ctrl.getDetail);
+router.get('/list', ctrl.getList);
+router.get('/detail', ctrl.getDetail);
 
 module.exports = router;
